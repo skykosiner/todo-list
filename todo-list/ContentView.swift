924ad1b0c6876b5ac3979309ebc034be55135421
yonikosiner <yoni@kosiner.co.uk>
2020-08-12 14:56:41 +0100

    install commit

---
+++ todo-list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-list
+//
+//  Created by Yoni Kosiner on 12/08/2020.
+//  Copyright © 2020 Yoni Kosiner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
